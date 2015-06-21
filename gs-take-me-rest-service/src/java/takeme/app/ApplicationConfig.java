bfd8676a5ab4be95e8e3854660763816a6ecd923
talza <zamirt@gmail.com>
2015-06-21 15:44:56 +0300

    Tal - commit

--- gs-take-me-rest-service/src/java/takeme/app/ApplicationConfig.java
+++ gs-take-me-rest-service/src/java/takeme/app/ApplicationConfig.java
@@ -3,6 +3,7 @@ package app;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaRepositories(basePackages="repositories")
 @EnableTransactionManagement
+@EnableAutoConfiguration
 public class ApplicationConfig {
 	
 	  @Bean
