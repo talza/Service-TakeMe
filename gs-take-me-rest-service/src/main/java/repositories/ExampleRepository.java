133d34c0284bd564dfc85cf819a0b6a3694732cd
avi_alima <avialima@gmail.com>
2015-08-04 22:11:40 +0300

    asdas

--- gs-take-me-rest-service/src/main/java/repositories/ExampleRepository.java
+++ gs-take-me-rest-service/src/main/java/repositories/ExampleRepository.java
@@ -2,7 +2,7 @@ package repositories;
 
 import java.util.Set;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import entities.ExampleEntity;
 
