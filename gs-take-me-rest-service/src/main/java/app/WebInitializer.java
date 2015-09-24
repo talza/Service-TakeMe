73318d23a443ce5a520df0c1d912a210f44900aa
avi_alima <avialima@gmail.com>
2015-09-24 22:27:53 +0300

    Config amazon db

--- gs-take-me-rest-service/src/main/java/app/WebInitializer.java
+++ gs-take-me-rest-service/src/main/java/app/WebInitializer.java
@@ -3,11 +3,16 @@ package app;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
-
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class WebInitializer extends SpringBootServletInitializer {
 
 
