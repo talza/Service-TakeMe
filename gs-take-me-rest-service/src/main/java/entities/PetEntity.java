f8a1a38c9ff339a465b5d983e0639175ae8aaf4d
Nofar Himani <Nofar.Himani@emc.com>
2015-11-07 12:55:41 +0200

    add json ignore to getAdEntity

--- gs-take-me-rest-service/src/main/java/entities/PetEntity.java
+++ gs-take-me-rest-service/src/main/java/entities/PetEntity.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="Pets")
 public class PetEntity extends BaseEntity<Long> {
@@ -75,6 +77,7 @@ public class PetEntity extends BaseEntity<Long> {
 		this.type = type;
 	}
 
+	@JsonIgnore
 	public AdEntity getAdEntity() {
 		return adEntity;
 	}
