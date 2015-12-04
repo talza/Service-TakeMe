9e48bb51219bd23f777e47dc21009e32db0fc81f
Nofar Himani <Nofar.Himani@emc.com>
2015-12-04 20:38:19 +0200

    remove unused imports

--- gs-take-me-rest-service/src/main/java/app/Application.java
+++ gs-take-me-rest-service/src/main/java/app/Application.java
@@ -1,13 +1,10 @@
 package app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"resources"})
--- gs-take-me-rest-service/src/main/java/app/WebInitializer.java
+++ gs-take-me-rest-service/src/main/java/app/WebInitializer.java
@@ -1,14 +1,10 @@
 package app;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Configuration
 @EnableAutoConfiguration
@@ -22,23 +18,5 @@ public class WebInitializer extends SpringBootServletInitializer {
     	
     	
     	return application.sources(Application.class);
-    }
-    
-//    @Override
-//    public void onStartup(ServletContext servletContext) throws ServletException {
-//    	FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
-//        encodingFilter.setInitParameter("encoding", "UTF-8");
-//        encodingFilter.setInitParameter("forceEncoding", "true");
-//        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
-//        super.onStartup(servletContext);
-//  }
-    
-//    @Bean
-//    CharacterEncodingFilter characterEncodingFilter() {
-//    	  CharacterEncodingFilter filter = new CharacterEncodingFilter();
-//    	  filter.setEncoding("UTF-8");
-//    	  filter.setForceEncoding(true);
-//    	  return filter;
-//    	}    
-
+    }   
 }
